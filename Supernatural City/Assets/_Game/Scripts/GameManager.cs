50bd67ffc29eb7f2b520e6d1f862fbc6640f38f6
Dooruss <doorusdebeijer@gmail.com>
2026-02-03 14:33:20 +0100

    Fix: Wrong scene

---
+++ Supernatural City/Assets/_Game/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int Day;
+    private int Year;
+    [Header("int Resources")]
+    private int Money;
+    private int Research_Points;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void UpdateUI()
+    {
+
+    }
+}
