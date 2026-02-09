3f11802f3ddc2359b2d02fdbbc61c4d0acd4a7c9
Dooruss <doorusdebeijer@gmail.com>
2026-02-09 11:09:46 +0100

    feat: ROAD

---
+++ Supernatural City/Assets/Large Roads/Tiled Tsx/Realistic Roads/Road_Asphalt_10-256x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Road_Asphalt_10-256x128" tilewidth="256" tileheight="128" tilecount="12" columns="4">
+ <image source="../Realistic Roads/Road_Asphalt_10-256x128.png" trans="ff00ff" width="1024" height="384"/>
+</tileset>
---
+++ Supernatural City/Assets/Large Roads/Tiled Tsx/Toon Roads/Road_Toon_09-256x128.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Road_Toon_09-256x128" tilewidth="256" tileheight="128" tilecount="12" columns="4">
+ <image source="../../Toon Roads/Road_Toon_09-256x128.png" trans="ff00ff" width="1024" height="384"/>
+</tileset>
