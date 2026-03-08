28bcdff7f15925d888c105ef3b70a277c92fccc6
Dooruss <doorusdebeijer@gmail.com>
2026-03-08 22:54:30 +0100

    Feat: NeedsRoad works

--- Supernatural City/Assets/_Game/Scripts/Managers/BuildManager.cs
+++ Supernatural City/Assets/_Game/Scripts/Managers/BuildManager.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -13,8 +15,14 @@ public class BuildManager : MonoBehaviour
     [SerializeField] public Building CurrentObject;
     [SerializeField] private Building RoadTile;
     [SerializeField] Camera Camera;
+    [SerializeField] private Building[] AllHousing;
     public bool BulldoseEnable;
     private GameManager gameManager;
+    Dictionary<Vector3Int, Building> PlacedBuildings = new Dictionary<Vector3Int, Building>();
+    // Hi Dorus from a couple days for now incase u forgot how this works
+    // Ty for unity discussions for this , So like It saves a Position (Vector3Int) and like an value on it (A building type)
+    // So like to save stuff its PlacedBuildings[PositionHere] = Building; (0,0,0) = Road (For example)
+    // And to remove its PlacedBuildings.Remove(PositionHere);
 
     private void Awake()
     {
@@ -23,7 +31,9 @@ public class BuildManager : MonoBehaviour
 
     private void Update()
     {
-        Vector3Int pos = CurrentTileMap.WorldToCell(Camera.ScreenToWorldPoint(Input.mousePosition));
+        Vector3 worldPos = Camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int pos = CurrentTileMap.WorldToCell(worldPos);
+        pos.z = 0;
 
         if (CurrentTile != null && Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
@@ -43,6 +53,12 @@ public class BuildManager : MonoBehaviour
             {
                 print(!CurrentTileMap.HasTile(Pos));
 
+                if (!CheckRoadNeed(Building, Pos))
+                {
+                    Debug.Log("Needs an road");
+                    return;
+                }
+
                 switch (Building.Type)
                 {
                     case Building.Building_Type.Road:
@@ -51,6 +67,7 @@ public class BuildManager : MonoBehaviour
                     case Building.Building_Type.Housing:
                         BuildItem(Pos, Building);
                         gameManager.Population += Building.People_Amount;
+                        gameManager.MoneyGeneration += Building.MoneyProduceAmount;
                         break;
                     case Building.Building_Type.Commercial:
                         //yay shops
@@ -71,6 +88,7 @@ public class BuildManager : MonoBehaviour
         Pos.z = 0;
         Pos = new Vector3Int(Pos.x, Pos.y, Pos.z);
         CurrentTileMap.SetTile(Pos, CurrentTile);
+        PlacedBuildings[Pos] = Building;
         gameManager.Money -= Building.Cost;
     }
 
@@ -80,8 +98,8 @@ public class BuildManager : MonoBehaviour
     void DeleteTile(Vector3Int Pos, Building Building)
     {
         Pos.z = 0;
-        Pos = new Vector3Int(Pos.x, Pos.y, Pos.z);
         CurrentTileMap.SetTile(Pos, null);
+        PlacedBuildings.Remove(Pos);
     }
 
     public void SetCurrentTile(Building Building)
@@ -101,15 +119,44 @@ public class BuildManager : MonoBehaviour
     }
     #endregion 
 
+    //Specific functions for 
     #region Building Specifics
-    private bool CheckRoadNeed(Building Building)
+    private bool CheckRoadNeed(Building Building, Vector3Int Position)
     {
-        if (Building.Need_Road)
+        if (!Building.Need_Road)
         {
-            //check if theres a road next to it
             return true;
-        } 
+        }
+
+        Vector3Int[] Directions = new Vector3Int[] {
+            new Vector3Int(1,0,0),
+            new Vector3Int(-1,0,0),
+            new Vector3Int(0,1,0),
+            new Vector3Int(0,-1,0)
+        };
+
+        foreach (Vector3Int Direction in Directions)
+        {
+            Vector3Int CheckPos = Position + Direction;
+            if (PlacedBuildings.TryGetValue(CheckPos, out Building neighbor))
+            {
+                if (neighbor.Type == Building.Building_Type.Road)
+                    return true;
+            }
+        }
+
         return false;
+    }
+
+    private int ProduceItem()
+    {
+        // Produce sutff
+        return 0;
+    }
+
+    private void CheckGroundType()
+    {
+
     }
 
     #endregion
