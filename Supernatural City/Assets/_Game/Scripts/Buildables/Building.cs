337cc3414dcab2503d5a94c23d058fd6595230ea
Dooruss <doorusdebeijer@gmail.com>
2026-03-23 11:02:59 +0100

    FIX: Better Assignments

--- Supernatural City/Assets/_Game/Scripts/Buildables/Building.cs
+++ Supernatural City/Assets/_Game/Scripts/Buildables/Building.cs
@@ -4,25 +4,30 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "Building", menuName = "Scriptable Objects/Building")]
 public class Building : ScriptableObject
 {
+    [Header("Main")]
     public string Building_Name;
     public bool Need_Road;
+    public TileBase Building_Sprite;
+    [SerializeField] public Building_Type Type;
+    [Header("Population & Money")]
+    [SerializeField] public Population_Type Population_Sort;
     public int People_Amount;
     public int Cost;
     public int UpKeepCost;
+    [SerializeField] public int MoneyProduceAmount;
     public int ResearchPointsAdded;
-    public TileBase Building_Sprite;
-    // Needs
+    [Header("Needs")]
     public int Need_Electricity;
     public int Need_Water;
     public int Need_Magic;
     public bool Functional;
-    // Producing?
+    [Header("Producing")]
     [SerializeField] public Product_Produced Producing;
-    [SerializeField] public Building_Type Type;
-    [SerializeField] public int MoneyProduceAmount;
+    
     [SerializeField] public int Product_Produce_Amount;
     public enum Product_Produced { None, Commercial, Electricity, Water, Magic }
     public enum Building_Type { Road, Housing, Commercial, Park, Generator }
+    public enum Population_Type { Witch , Vampire}
 
 
 
